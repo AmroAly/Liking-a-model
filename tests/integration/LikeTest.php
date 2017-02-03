fd58380458e3900cf58554d5d27bf2da5e8ae272
Amr Aly <amr.aly57@yahoo.com>
2017-02-03 18:59:57 +0200

    added the signin into the signup method

--- tests/integration/LikeTest.php
+++ tests/integration/LikeTest.php
@@ -15,12 +15,13 @@ class LikeTest extends TestCase
         parent::setUp();
 
         $this->post = factory(App\Post::class)->create();
+
+        $this->signIn();
     }
     
     /** @test */
     public function a_user_can_like_a_post()
     {
-    	$this->signIn();
     	
     	// when they like a post
     	$this->post->like();
@@ -36,20 +37,14 @@ class LikeTest extends TestCase
 
     /** @test */
     public function a_user_can_unlike_a_post()
-    {
-    	// and a user
-    	$user = factory(App\User::class)->create();
-    	
-    	// and that user is logged in
-    	$this->actingAs($user);
-    	
+    {	
     	// when they like a post
     	$this->post->like();
 
     	$this->post->unlike();
     	// then we should see evidence in the database
     	$this->notSeeInDatabase('likes', [
-    		'user_id' => $user->id,
+    		'user_id' => $this->user->id,
     		'likeable_id' => $this->post->id,
     		'likeable_type'	=> get_class($this->post)
     	]);
@@ -59,12 +54,7 @@ class LikeTest extends TestCase
 
     public function a_user_may_toggle_a_posts_like_status()
     {
-    	// and a user
-    	$user = factory(App\User::class)->create();
-    	
-    	// and that user is logged in
-    	$this->actingAs($user);
-    	
+     	
     	$this->post->toggle();
     	$this->assertTrue($this->post->isLiked());
 
